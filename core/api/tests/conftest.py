9a72125b1fbac753adf3104bc4a29e382189265d
Sidnei Pereira <sidnei@thegoodfellas.com.br>
2018-04-12 17:39:22 -0300

    Add test for list employee API's method.

--- core/api/tests/conftest.py
+++ core/api/tests/conftest.py
@@ -1,8 +1,18 @@
 import pytest
 
-from .factories import DepartmentFactory
+from .factories import DepartmentFactory, EmployeeFactory
 
 
 @pytest.fixture
 def department():
     return DepartmentFactory.create()
+
+
+@pytest.fixture
+def employee(department):
+    return EmployeeFactory.create(department=department)
+
+
+@pytest.fixture
+def employees(department):
+    return EmployeeFactory.create_batch(2, department=department)
--- core/api/tests/test_views.py
+++ core/api/tests/test_views.py
@@ -16,3 +16,12 @@ def test_create_employee(client, department):
     assert response.status_code == 201
     last_inserted_employee = Employee.objects.last()
     assert last_inserted_employee.email == employee.email
+
+
+@pytest.mark.django_db
+def test_list_employee(client, employees):
+    expected_amount = Employee.objects.all().count()
+    response = client.get('/employee/')
+    assert response.status_code == 200
+    amount = len(response.json())
+    assert expected_amount == amount
