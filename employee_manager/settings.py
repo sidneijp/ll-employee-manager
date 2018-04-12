de82629ce7ba8a25bdefaeddecc2e7476d2e53bb
Sidnei Pereira <sidnei@thegoodfellas.com.br>
2018-04-12 19:43:01 -0300

    Enable authentication for API.

--- employee_manager/settings.py
+++ employee_manager/settings.py
@@ -132,6 +132,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
     ),
